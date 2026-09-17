87a86882d8bd381cd9006d8e17221571ff788463
Yusuf Ender Osmanoğlu <yusufenderosmanoglu@gmail.com>
2026-09-17 18:08:12 +0300

    finish stdlib string tests

--- tests/test.lua
+++ tests/test.lua
@@ -1,24 +1,25 @@
 local Testy = require "testy"
 
-local test = Testy
-    .init("Cole_Test")
+local suites = {
+    ["stdlib"] = {
+        "string"
+    },
+}
 
-    :suite((function()
-        local files = {
-            "string"
-        }
+local function suite(name, tests)
+    local suite = Testy.Suite.init(name)
+    for _, test in ipairs(tests) do
+        local execp = "build/c/"..test..".cole.exec"
+        suite:test(Testy.Test
+            .init(test, execp))
+    end
+    return suite
+end
 
-        local suite = Testy.Suite.init("stdlib", "")
-
-        for _, file in ipairs(files) do
-            local execp = "build/c/"..file..".cole.exec"
-
-            suite:test(Testy.Test
-                .init(file, execp))
-        end
-
-        return suite
-    end)())
+local test = Testy.init("Cole_Test")
+for name, tests in pairs(suites) do
+    test:suite(suite(name, tests))
+end
 
 if arg[1] == "record" then
     test:record()
